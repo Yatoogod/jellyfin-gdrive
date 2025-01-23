2a24a55fdcb698f0d2fba5250bbf163a3f85ed15
Yatoogod <email.yamann@gmail.com>
2025-01-23 12:14:56 +0530

    Update and rename mount-gdrive.sh to entrypoint.sh

---
+++ entrypoint.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+/mount-gdrive.sh
+/jellyfin/jellyfin
--- mount-gdrive.sh
+++
@@ -1,9 +0,0 @@
-# mount-gdrive.sh
-#!/bin/bash
-mkdir -p /media/gdrive
-rclone mount gdrive:/ /media/gdrive --allow-other &
-
-# entrypoint.sh
-#!/bin/bash
-/mount-gdrive.sh
-/jellyfin/jellyfin
