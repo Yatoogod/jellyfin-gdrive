a51013ffdd70bc95efae3fbff64561929e1d8274
Yatoogod <email.yamann@gmail.com>
2025-01-23 12:50:15 +0530

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
-/mount-gdrive.sh
-/jellyfin/jellyfin
+
+# Mount Google Drive using Rclone
+/mount-gdrive.sh &
+
+# Wait a bit to ensure the mount is ready
+sleep 5
+
+# Start Jellyfin
+exec /jellyfin/jellyfin "$@"
