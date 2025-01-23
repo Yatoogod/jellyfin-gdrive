337b93f2f22c1aaadc5f0d5f95cfe9eb6146c4a3
Yatoogod <email.yamann@gmail.com>
2025-01-23 13:03:00 +0530

    Update mount-gdrive.sh

--- mount-gdrive.sh
+++ mount-gdrive.sh
@@ -1,10 +1,4 @@
-#!/bin/bash
-
-# Mount Google Drive to a specific directory
 rclone mount gdrive: /mnt/gdrive \
     --vfs-cache-mode writes \
     --allow-other \
-    --daemon
-
-# Log successful mount
-echo "Google Drive mounted at /mnt/gdrive"
+    -vv --log-file=/var/log/rclone-debug.log
